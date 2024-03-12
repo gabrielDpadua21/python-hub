67b7b3914d0a1fde05d2655347a71ea1e93577c7
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2024-03-11 23:39:39 -0300

    feat: ✨ palindrome number

---
+++ code-challenges/src/leetcode/easy/palindrome_number.py
@@ -0,0 +1,19 @@
+
+
+class PalindromeNumber:
+
+	def solution01(self, number):
+		return str(number) == "".join(reversed(str(number)))
+
+	def solution02(self, number):
+		if number < 0: return False
+
+		reversed_num = 0
+		temp = number
+
+		while temp != 0:
+			digit = temp % 10
+			reversed_num = reversed_num * 10 + digit
+			temp //= 10
+
+		return reversed_num == number 
